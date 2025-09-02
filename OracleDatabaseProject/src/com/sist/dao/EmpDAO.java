fedeceaddbcb1230993347743145c2315c3d923a
dongdongsw <fishman4535@gmail.com>
2025-09-02 17:31:17 +0900

    2일차

---
+++ OracleDatabaseProject/src/com/sist/dao/EmpDAO.java
@@ -0,0 +1,155 @@
+package com.sist.dao;
+
+import java.util.*;
+
+import com.sist.vo.EmpVO;
+
+import java.sql.*;
+
+/*
+ *	DAO => 데이터베이스 연동
+ *	VO	=> 컬럼 값을 받기 위한 자바 변수 설정 
+ *	--------------------------------
+ *	오라클은 실행과정 =>	ROW단위로
+ *
+ *	DAO 제작
+ *	1. 오라클 드라이버 설치 : ojdbc8.jar => 오라클사에 제공
+ *		Class.forName("드라이버") => 한번 (생성자)
+ *	2. 데이터베이스 연결
+ *		Connection
+ *	3. 데이터베이스 해제
+ *	4. SQL 문장 제작 => 
+ *	5. 오라클 전송
+ * 	6. 오라클에서 SQL 문장 실행 => 결과값을 얻어 온다
+ * 	7. 결과 값을 List/VO에 담아서 => 콘솔 / 윈도우 / 브라우저에 전송
+ * 	8. 닫기
+ * 	---------------------------------------메소드 한개로 제작
+ * 		SelectList	SelectOne
+ */
+
+public class EmpDAO {
+	//1. 공통 사용된 객체
+	private Connection conn; // 오라클 연결 객체
+	// => SQL문자 ANSI(표준화)
+	
+	// 2. SQL 전송
+	private PreparedStatement ps;
+	
+	// 3. 싱글톤
+	private static EmpDAO dao;
+	
+	// 4. 오라클 주소
+	private final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+	
+	// 5. 드라이버 등록
+	public EmpDAO() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			//com.mysql.cj.Driver
+		}
+		catch(Exception ex) {}
+		
+	}
+	// 6. 싱글톤 구현
+	public static EmpDAO newInstance() {
+		if(dao == null) {
+			dao = new EmpDAO();
+		}
+
+		return dao;
+	}
+	
+	// 7. 오라클 연결
+	public void getConnection() {
+		try {
+			conn = DriverManager.getConnection(URL, "hr","happy");
+		}
+		catch(Exception ex) {}
+
+	}
+	// 8. 오라클 연결 종료
+	public void disConnection() {
+		try {
+			if(ps != null) {
+				ps.close();
+			}
+			if(conn != null) {
+				conn.close();
+			}
+		}
+		catch(Exception ex) {}
+
+	}
+	// 9. 기능
+	// 9-1. 목록 => 프로젝션 List
+	public List<EmpVO> empListData(){
+		
+		List<EmpVO> list = new ArrayList<EmpVO>();
+		
+		try {
+			// 1. 연결
+			getConnection();
+			
+			// 2. 오라클에 어떤 문장을 전송할지
+			String sql = "SELECT empno, ename, job, TO_CHAR(hiredate,'YYYY-MM-DD'),sal FROM emp ORDER BY sal DESC";
+			
+			// 3. 오라클로 전송
+			ps = conn.prepareStatement(sql);
+			
+			// 4. 실행 후에 결과 값을 메모리에 저장
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				
+				EmpVO vo = new EmpVO();
+				vo.setEmpno(rs.getInt(1));
+				vo.setEname(rs.getString(2));
+				vo.setJob(rs.getString(3));
+				vo.setDbday(rs.getString(4));
+				vo.setSal(rs.getInt(5));
+				
+				list.add(vo);
+				
+			}
+			
+			// 5. 메모리 닫기
+			rs.close();
+		}
+		catch(Exception ex) {
+			
+			ex.printStackTrace();
+			
+		}
+		finally {
+			
+			disConnection();
+			
+		}
+		return list;
+		
+	}
+	// 9-2. 상세보기 => 셀렉션 VO
+	// 9-3. 검색	=> LIKE List
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
